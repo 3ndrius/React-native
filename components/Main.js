ac77fbdde7f9438942d062ef41c7eec80bd91140
3ndrius <andrzejo1991@gmail.com>
2018-11-23 18:45:22 +0100

    Adding stylesheet and adding new component

--- components/Main.js
+++ components/Main.js
@@ -25,6 +25,9 @@ export default class Main extends Component {
           underlineColorAndroid='transparent'>
           </TextInput>
         </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.addButtonText} > + </Text>
+        </TouchableOpacity>
      </View>
     )
   }
@@ -33,6 +36,55 @@ export default class Main extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#DCDCDD'
 
   },
+  header: {
+    backgroundColor: '#1985A1',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  headerText: {
+    color: '#DCDCDD',
+    fontSize: 20,
+    padding: 20,
+    fontWeight: '700',
+    marginTop: 20
+  },
+  listContainer: {
+    flex: 1,
+    marginBottom: 100
+  },
+  footer: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex:10,
+   
+  },
+  inputText: {
+    alignSelf: 'stretch',
+    color: '#DCDCDD',
+    padding: 25,
+    backgroundColor: '#46494C'
+  },
+  button: {
+    height:60,
+    width:60,
+    backgroundColor:'#1985A1',
+    position: 'absolute',
+    zIndex: 11,
+    bottom: 40,
+    right:20,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  addButtonText: {
+    fontWeight: '700',
+    fontSize: 25,
+    color:'#DCDCDD'
+    
+  }
 });
